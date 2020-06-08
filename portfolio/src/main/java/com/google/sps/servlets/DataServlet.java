e0aa81a48f3168b40a29d56a93df9d23d6b5f77c
Gabrielle Germanson <gabbygermanson@gmail.com>
2020-06-08 20:05:07 +0000

    Implement user capability to specify number of site comments to be seen

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -10,9 +10,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
@@ -27,70 +27,107 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 /** Servlet that returns a random quote. */
 @WebServlet("/data")
 public final class DataServlet extends HttpServlet {
+ 
+    /** Declared as class variable so that initialization happens once in init()
+        rather than everytime in doGET() */
+    List<String> funFacts = new ArrayList<>();
     
-    /** One time initialization may be used for future. */
-    // @Override
-    // public void init() {
-    // }
-
-
-    /** Merges dataFactsAndComments with pastComments to convert as one ArrayList into JSON to put on servlet. */
+    /** One time initialization. */
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List<String> funFacts = new ArrayList<>();
+    public void init() {
         funFacts.add("I enjoy all music genres especially EDM!");
         funFacts.add("I studied abroad in Barcelona :)");
         funFacts.add("My favorite food is rotisserie chicken!");
         funFacts.add("I spend a lot of days on the lake!");
+    }
+ 
+    /** Merges dataFactsAndComments with pastComments to convert as one ArrayList into JSON to put on servlet. */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {        
+        // Add fun facts first to factsAndComments return variable.
+        List<String> factsAndComments = new ArrayList<>();
+        factsAndComments.addAll(funFacts);        
         
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Query query = new Query("siteComments").addSort("timeStamp", SortDirection.DESCENDING);
         PreparedQuery results = datastore.prepare(query);
-
+ 
         List<String> pastComments = new ArrayList<>();
+        int validNumRequest = 0;
+        int numCommentsWanted = 0;
+        boolean numCommentsWantedTrigger = true;
+ 
         for (Entity entity : results.asIterable()) {
-            String comment = (String) entity.getProperty("newComment");
-            pastComments.add(comment);
+            validNumRequest = Integer.parseInt((String)(entity.getProperty("seeNumComments")));
+            if (validNumRequest >= 0){
+                String comment = (String) (entity.getProperty("newComment"));
+
+                //For testing queried result comments
+                // System.out.println("Query result comment: " + comment);
+
+                pastComments.add(comment);
+            } else {
+                System.out.println("Input of number comments wanted not greater than or equal to 0: " + validNumRequest);
+            }
+           
+            //Grab number of comments wanted to be seen by user only in the first entity
+            while (numCommentsWantedTrigger) {
+                numCommentsWanted = validNumRequest;
+                numCommentsWantedTrigger = false;
+            }
         }
+ 
+        if (numCommentsWanted > pastComments.size()) {
+            numCommentsWanted = pastComments.size();           
+        }
+ 
+        pastComments = pastComments.subList(0, numCommentsWanted);
 
-        List<String> factsAndComments =  new ArrayList<>();
-        factsAndComments.addAll(funFacts);
+        // For testing proper number and right comments are in sublist
+        // for (String comment : pastComments) {
+        //     System.out.println("sublist comment: " + comment);
+        // }
+
         factsAndComments.addAll(pastComments);
-
+ 
         response.setContentType("application/json;");
         Gson gson = new Gson();
         response.getWriter().println(gson.toJson(factsAndComments));
     }
-
+ 
     /** Process POST request form input, add to pastComments, and redirect to index.html */
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Get the input from the form.
-        String comment = getParameter(request, "text-input", "");
         long timestamp = System.currentTimeMillis();
-
+        
+        // Get the input from the form.
+        String comment = getParameter(request, "text-input", ""); 
+        String numCommentsString = getParameter(request, "num-comments", "0");
+ 
         Entity commentEntity = new Entity("siteComments");
-        commentEntity.setProperty("newComment", comment);
         commentEntity.setProperty("timeStamp", timestamp);
-
+        commentEntity.setProperty("newComment", comment);
+        commentEntity.setProperty("seeNumComments", numCommentsString);
+ 
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(commentEntity);
-
+ 
         response.sendRedirect("/index.html");
     }
-
+ 
     /** Gets form input of specified type
     * @return request parameter or default value if parameter was not specified by client */
     private String getParameter(HttpServletRequest request, String name, String defaultValue) {
         String value = request.getParameter(name);
         if (value == null) {
+            System.out.println("Could not convert number comments wanted input to int: " + value);
             return defaultValue;
         }
         return value;
     }
-
+ 
 }
